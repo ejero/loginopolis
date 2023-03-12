e87eb2a178768460fe0a824c4c039845d892ec73
Rosita E <RositaEmakpo@Gmail.com>
2023-03-12 13:32:33 -0700

    Adds post register and login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require('bcrypt'); 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -15,10 +16,42 @@ app.get('/', async (req, res, next) => {
 });
 
 // POST /register
+app.post('/register', async (req, res) => {
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+const {username, password} = req.body;
+try{
+    const hashedpw = await bcrypt.hash(password, 10);
+    const createdUser = await User.create({username, password: hashedpw});
+    res.status(201).json(createdUser);
+  } catch(err){
+    console.log(err);
+    res.status(500).json({ message: 'Failed to create user.' });
+  }
+})
 
 // POST /login
-// TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post("/login", async(req, res, Next) => {
+  // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+  try{
+    const { username, password } = req.body;
+    const foundUser = await User.findOne({where: {username}});
+    if(!foundUser){
+      res.status(400).send("Failed login");
+      return;
+    }
+    const isMatch = await bcrypt.compare(password, foundUser.password);
+
+    if(isMatch){
+      res.status(400).send("Failed login");
+    }
+    res.send("Success")
+    
+  } catch(err){
+    console.error(err)
+    res.status(500).json({ message: 'Failed to log in.' });
+  }
+  res.send("Success");
+})
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
